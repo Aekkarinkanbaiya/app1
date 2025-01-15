2ffea2b1a38740022f6e55cb6d0ce063cbc8a05f
Aekkarinkanbaiya <tawangold78@gmail.com>
2025-01-15 15:58:16 +0700

    Commit Add state-func.js
    Commit Add state-func.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import { EventData2 } from './event-data';
 import RefsFunc from './refs-func';
+import Messagebox from './state-func';
 
 function App() {
-  return <RefsFunc/>
+  return <Messagebox/>
 }
 
 export default App;
