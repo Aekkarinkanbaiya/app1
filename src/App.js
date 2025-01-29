664d53a3553748fa8a82089cea6d465f8957360d
Aekkarinkanbaiya <tawangold78@gmail.com>
2025-01-29 14:19:11 +0700

    Commit

--- src/App.js
+++ src/App.js
@@ -2,9 +2,15 @@ import React from 'react'
 import { EventData2 } from './event-data';
 import RefsFunc from './refs-func';
 import Messagebox from './state-func';
+import { userContext } from './context';
+import Content from './context-content';
 
 function App() {
-  return <Messagebox/>
+  return (
+    <userContext.Provider value={'Tom Jerry'}>
+      <Content/>
+    </userContext.Provider>
+  )
 }
 
 export default App;
