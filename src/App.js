420045c1febd88fc8f000c1a24f12b940e314d41
Aekkarinkanbaiya <tawangold78@gmail.com>
2025-02-26 18:56:01 +0700

    Commit Add FormSelectFile.js
    Commit Add FormSelectFile.js

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import React from 'react'
 import Router2 from './router2';
 import Router3 from './router3';
 import FormText from './form-text';
+import FormSelectFile from './form - select-file';
 
 function App() {
-  return <FormText/>
+  return <FormSelectFile/>
 
 }
 
